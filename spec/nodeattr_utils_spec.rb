1e3670b6ca679ca4ccd7cb42a0617579696f2def
william <william.mccumstie@outlook.com>
2019-01-18 12:05:14 +0000

    Remove the Gemfile.lock and the dummy spec
    The lock file shouldn't be checked in as this is a shared library. And
    the spec did nothing

--- spec/nodeattr_utils_spec.rb
+++ spec/nodeattr_utils_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe NodeattrUtils do
   it "has a version number" do
     expect(NodeattrUtils::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
