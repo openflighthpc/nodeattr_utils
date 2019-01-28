39c31ebde582f6dc45d638a6749fe4b022b2b084
william <william.mccumstie@outlook.com>
2019-01-28 18:17:08 +0000

    Reformat the spec file so it is more usable
    The shared example will eventually have a `::collapse` method added. For
    this to fit into the spec, the `::expand` describe needed to be nested

--- spec/nodeattr_utils/node_parser_spec.rb
+++ spec/nodeattr_utils/node_parser_spec.rb
@@ -35,11 +35,109 @@ RSpec.describe NodeattrUtils::NodeParser do
     (indices.first..indices.last)
   end
 
-  describe '::expand' do
-    def expect_expand(*args)
-      expect(described_class.expand(*args))
+  def expect_expand(*args)
+    expect(described_class.expand(*args))
+  end
+
+  shared_examples 'parses the nodes' do
+    describe '::expand' do
+      it 'expands the nodes' do
+        expect_expand(nodes_string).to contain_exactly(*nodes)
+      end
+    end
+  end
+
+  context 'with a single node input' do
+    ['node', 'node1', 'node01'].each do |node|
+      let(:nodes_string) { node }
+      let(:nodes) { [node] }
+
+      include_examples 'parses the nodes'
+    end
+  end
+
+  context 'with a continuous, non-leading zero range' do
+    let(:indices) { [0, 20] }
+    let(:nodes) { index_range(indices).map { |i| "node#{i}" } }
+    let(:nodes_string) { "node[#{indices.first}-#{indices.last}]" }
+
+    include_examples 'parses the nodes'
+  end
+
+  context 'with a continuous, leading zero range' do
+    let(:indices) { [0, 20] }
+    let(:nodes) do
+      index_range(indices).map { |i| "node#{pad_int(i)}" }
+    end
+    let(:nodes_string) do
+      "node[#{pad_int(indices.first)}-#{pad_int(indices.last)}]"
     end
 
+    include_examples 'parses the nodes'
+  end
+
+  context 'with a discrete range of nodes' do
+    let(:indices) { ['0', '00', '1', '02', '3', '4', '6', '007'] }
+    let(:nodes) { indices.map { |i| "node#{i}" } }
+    let(:nodes_string) { "node[#{indices.join(',')}]" }
+
+    include_examples 'parses the nodes'
+  end
+
+  context 'with a complex leading zero, continuous range' do
+    let(:width) { 3 }
+    let(:indices) { [4, 19] }
+    let(:nodes) do
+      index_range(indices).map { |i| "node#{pad_int(i, width: width)}" }
+    end
+    let(:nodes_string) do
+      # The first leading zero is outside the range
+      min = pad_int(indices.first, width: width - 1)
+      max = pad_int(indices.last, width: width - 1)
+      "node0[#{min}-#{max}]"
+    end
+
+    include_examples 'parses the nodes'
+  end
+
+  context 'with a compound continuous/discrete range' do
+    let(:cont_indices) { [9, 101] }
+    let(:cont_nodes) do
+      index_range(cont_indices).map { |i| "node#{pad_int(i)}" }
+    end
+    let(:cont_str) do
+      "#{pad_int(cont_indices.first)}-#{pad_int(cont_indices.last)}"
+    end
+
+    let(:prefix_indices) { [103, 105, 107, 109] }
+    let(:suffix_indices) { [1, 2, 4, 5, 8] }
+    # NOTE: The prefix/suffix are out of order here b/c it shouldn't make
+    # a difference to the spec
+    let(:discrete_nodes) do
+      [suffix_indices, prefix_indices].flatten
+                                      .map { |i| "node#{i}" }
+    end
+
+    let(:nodes) do
+      [cont_nodes, discrete_nodes].flatten
+    end
+    let(:nodes_string) do
+      prefix = prefix_indices.join(',')
+      suffix = suffix_indices.join(',')
+      "node[#{prefix},#{cont_str},#{suffix}]"
+    end
+
+    include_examples 'parses the nodes'
+  end
+
+  context 'with multiple single nodes' do
+    let(:nodes) { ['node', 'slave', 'login'] }
+    let(:nodes_string) { nodes.join(',') }
+
+    include_examples 'parses the nodes'
+  end
+
+  describe '::expand' do
     context 'with a invalid node input' do
       [
         '%%%', 'n[1-9', 'n4]', 'n[c]', 'n[1,c,2]', 'n[2-1]', 'n[-2]', 'n[]',
@@ -53,107 +151,12 @@ RSpec.describe NodeattrUtils::NodeParser do
       end
     end
 
-    shared_examples 'expands the nodes' do
-      it 'expands the nodes' do
-        expect_expand(nodes_string).to contain_exactly(*nodes)
-      end
-    end
-
     context 'with a empty inputs' do
       ['', nil].each do |node|
         it "#{node.inspect} returns an empty array" do
           expect_expand(node).to eq([])
         end
       end
-    end
-
-    context 'with a single node input' do
-      ['node', 'node1', 'node01'].each do |node|
-        it "returns: [#{node}]" do
-          expect_expand(node).to contain_exactly(node)
-        end
-      end
-    end
-
-    context 'with a continuous, non-leading zero range' do
-      let(:indices) { [0, 20] }
-      let(:nodes) { index_range(indices).map { |i| "node#{i}" } }
-      let(:nodes_string) { "node[#{indices.first}-#{indices.last}]" }
-
-      include_examples 'expands the nodes'
-    end
-
-    context 'with a continuous, leading zero range' do
-      let(:indices) { [0, 20] }
-      let(:nodes) do
-        index_range(indices).map { |i| "node#{pad_int(i)}" }
-      end
-      let(:nodes_string) do
-        "node[#{pad_int(indices.first)}-#{pad_int(indices.last)}]"
-      end
-
-      include_examples 'expands the nodes'
-    end
-
-    context 'with a discrete range of nodes' do
-      let(:indices) { ['0', '00', '1', '02', '3', '4', '6', '007'] }
-      let(:nodes) { indices.map { |i| "node#{i}" } }
-      let(:nodes_string) { "node[#{indices.join(',')}]" }
-
-      include_examples 'expands the nodes'
-    end
-
-    context 'with a complex leading zero, continuous range' do
-      let(:width) { 3 }
-      let(:indices) { [4, 19] }
-      let(:nodes) do
-        index_range(indices).map { |i| "node#{pad_int(i, width: width)}" }
-      end
-      let(:nodes_string) do
-        # The first leading zero is outside the range
-        min = pad_int(indices.first, width: width - 1)
-        max = pad_int(indices.last, width: width - 1)
-        "node0[#{min}-#{max}]"
-      end
-
-      include_examples 'expands the nodes'
-    end
-
-    context 'with a compound continuous/discrete range' do
-      let(:cont_indices) { [9, 101] }
-      let(:cont_nodes) do
-        index_range(cont_indices).map { |i| "node#{pad_int(i)}" }
-      end
-      let(:cont_str) do
-        "#{pad_int(cont_indices.first)}-#{pad_int(cont_indices.last)}"
-      end
-
-      let(:prefix_indices) { [103, 105, 107, 109] }
-      let(:suffix_indices) { [1, 2, 4, 5, 8] }
-      # NOTE: The prefix/suffix are out of order here b/c it shouldn't make
-      # a difference to the spec
-      let(:discrete_nodes) do
-        [suffix_indices, prefix_indices].flatten
-                                        .map { |i| "node#{i}" }
-      end
-
-      let(:nodes) do
-        [cont_nodes, discrete_nodes].flatten
-      end
-      let(:nodes_string) do
-        prefix = prefix_indices.join(',')
-        suffix = suffix_indices.join(',')
-        "node[#{prefix},#{cont_str},#{suffix}]"
-      end
-
-      include_examples 'expands the nodes'
-    end
-
-    context 'with multiple single nodes' do
-      let(:nodes) { ['node', 'slave', 'login'] }
-      let(:nodes_string) { nodes.join(',') }
-
-      include_examples 'expands the nodes'
     end
   end
 end
